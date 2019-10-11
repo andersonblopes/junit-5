85d7b08d526098e9e053433b2c84e829939bf753
andersonlopes <andersonlopes@imacdeanderson.lan>
2019-10-11 11:29:41 +0100

    Test assertArrayEquals

--- test/com/lopes/junit5/StringTest.java
+++ test/com/lopes/junit5/StringTest.java
@@ -1,5 +1,6 @@
 package com.lopes.junit5;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +23,14 @@ class StringTest {
 	@Test
 	void contains_basic() {
 		assertFalse("abcdefgh".contains("ijk"));
+	}
+	
+	@Test
+	void split_basic() {
+		String str = "abc def ghi";
+		String[] actualResult = str.split(" ");
+		String[] expectedResult = new String[] {"abc","def","ghi"};
+		assertArrayEquals(actualResult, expectedResult);
 	}
 
 }
