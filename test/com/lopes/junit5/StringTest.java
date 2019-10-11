e5a2de6e0850174f37f8bc9dd6d5e3e53e0bce63
andersonlopes <andersonlopes@imacdeanderson.lan>
2019-10-11 11:01:59 +0100

    Test assertNull and assertTrue.

--- test/com/lopes/junit5/StringTest.java
+++ test/com/lopes/junit5/StringTest.java
@@ -1,5 +1,7 @@
 package com.lopes.junit5;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -7,12 +9,27 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
 	@Test
-	void testLenght() {
+	void length_basic() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
 		assertEquals(expectedLength, actualLength);
-		
-		
 	}
+	
+	@Test
+	void toUpperCase_basic() {
+		String str = "abcd";
+		String result = str.toUpperCase();
+		assertNotNull(result);
+		assertEquals("ABCD", result);
+	}
+	
+	@Test
+	void contains_basic() {
+		String str = "abcdefgh";
+		boolean result = str.contains("ijk");
+		assertFalse(result);
+	}
+	             
+			
 
 }
