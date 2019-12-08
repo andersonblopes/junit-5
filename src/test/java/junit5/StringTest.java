df83cdd31c6ff57c0cdcf1ce06cc4a4b4b574bf2
Anderson Lopes <anderson.lopes@gmail.com>
2019-12-08 15:06:41 +0000

    Includes javadoc.

--- src/test/java/junit5/StringTest.java
+++ src/test/java/junit5/StringTest.java
@@ -7,24 +7,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * The type String test.
+ */
 class StringTest {
 
+	/**
+	 * Length basic.
+	 */
 	@Test
 	void length_basic() {
 		assertEquals(4, "ABCD".length());
 	}
-	
+
+	/**
+	 * To upper case basic.
+	 */
 	@Test
 	void toUpperCase_basic() {
 		assertNotNull("abcd".toUpperCase());
 		assertEquals("ABCD", "abcd".toUpperCase());
 	}
-	
+
+	/**
+	 * Contains basic.
+	 */
 	@Test
 	void contains_basic() {
 		assertFalse("abcdefgh".contains("ijk"));
 	}
-	
+
+	/**
+	 * Split basic.
+	 */
 	@Test
 	void split_basic() {
 		String str = "abc def ghi";
